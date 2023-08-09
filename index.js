c9d3cae4122054c828dcfc50cc521a5dbbafa931
ismailcubuk <iismailcubuk@gmail.com>
2023-08-09 09:16:00 +0300

    delete post

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const db = require('./config/database.js');
-const Post = require('./routes/post.js');
 const Phone = require('./routes/phone.js'); // Import the Phone router
 const Shoes = require('./routes/shoes.js'); // Import the Shoes router
 
@@ -16,7 +15,6 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 // Routes
-app.use('/posts', Post);
 app.use('/phones', Phone); // Use the Phone router for phone-related routes
 app.use('/shoes', Shoes); // Use the Phone router for phone-related routes
 
