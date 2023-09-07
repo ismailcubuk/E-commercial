506d566a5e1fe4419b06400f76d0e8173106f3f2
ismailcubuk <iismailcubuk@gmail.com>
2023-09-07 21:12:45 +0300

    add userprofile

--- client/pages/profile/profile.tsx
+++ client/pages/profile/profile.tsx
@@ -1,21 +1,19 @@
 import React from "react";
-
-import { Avatar, Badge } from "@nextui-org/react";
 import { Container, Grid, Paper, Typography } from "@mui/material";
 import { CustomButton } from "@/components/CustomButton";
-import PhotoCameraOutlinedIcon from '@mui/icons-material/PhotoCameraOutlined';
 import FirstName from "@/components/Forms/Profile/FirstName";
 import SurName from "@/components/Forms/Profile/SurName";
 import Mail from "@/components/Forms/Profile/Mail";
 import Password from "@/components/Forms/Profile/Password";
 import Navigation from "@/components/Layout/Profile/Navigation";
 import Main from "@/components/Layout/Profile/Main";
+import UserProfile from "@/components/UserProfile";
 
 export default function profile() {
 
   const iconClasses =
     "text-xl text-default-500 pointer-events-none flex-shrink-0";
-  const avatarImage = "https://i.pravatar.cc/150?u=a042581f4e29026024d";
+
 
 
   return (
@@ -25,12 +23,8 @@ export default function profile() {
         <Navigation />
         {/* MAİN */}
         <Main variant="profile">
-          <div className="flex gap-5 items-center ">
-            <Badge content={<PhotoCameraOutlinedIcon />} className="w-10 h-10" placement="bottom-right" color="default">
-              <Avatar src={avatarImage} radius='md' className="w-20 h-20" />
-            </Badge>
-            <Typography variant="h4"> İsmail çubuk</Typography>
-          </div>
+          {/* USER PROFİLE */}
+          <UserProfile />
           <Grid container spacing={2} mt={2} mb={4}>
             {/* İlk grup */}
             <Grid item xs={12} >
