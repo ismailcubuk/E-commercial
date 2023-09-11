02f8299c0428cf313406f1b33cf43304af7b07ad
ismailcubuk <iismailcubuk@gmail.com>
2023-09-11 08:25:58 +0300

    delete fetch dont need

--- client/redux/store/store.ts
+++ client/redux/store/store.ts
@@ -1,29 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from '../reducers/menuReducer';
-import headphonesReducer from "../slices/headphonesSlice";
-import headsetsReducer from "../slices/headsetsSlice";
-import keyboardsReducer from "../slices/keyboardsSlice";
-import mouseReducer from "../slices/mouseSlice";
-import phonesReducer from "../slices/phonesSlice";
-import shoesReducer from "../slices/shoesSlice";
-import tracksuitsReducer from "../slices/tracksuitsSlice";
-import tshirtsReducer from "../slices/tshirtsSlice";
-import watchesReducer from "../slices/watchesSlice";
 import alldataReducer from "../slices/alldataSlice";
 
 export const store = configureStore({
   reducer: {
     alldata: alldataReducer,
     toggleMenu: menuReducer,
-    headphones: headphonesReducer,
-    headsets: headsetsReducer,
-    keyboards: keyboardsReducer,
-    mouse: mouseReducer,
-    phones: phonesReducer,
-    shoes: shoesReducer,
-    tracksuits: tracksuitsReducer,
-    tshirts: tshirtsReducer,
-    watches: watchesReducer,
   },
 });
 
